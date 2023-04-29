650614ebbe9089bb7bc534ca10f626bcb3b5ddc1
Irvan Rifaí <ivan.rivai6921@gmail.com>
2023-04-29 16:59:32 +0700

    fetched one row

--- getDataPendampingHalal.py
+++ getDataPendampingHalal.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+import pandas as pd
+import time
 import csv
 
 # link to scrap
@@ -15,29 +17,71 @@ url = "https://info.halal.go.id/pendampingan/"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+
 # make request with selenium
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.get(url)
 
+time.sleep(0.4)
 driver.implicitly_wait(20)
 # Get button and click it
+WebElement
 btn_click = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.ID, "GridView3_lbView_0")))
+driver.execute_script("arguments[0].click();", btn_click)
+# btn_click = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH, "//div[contains(@id, 'GridView3_lbView_0')]//span//following::a[1]")))
+btn_click = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH, "//a[contains(@id,'GridView3_lbView_0')]")))
 driver.execute_script("arguments[0].click();", btn_click)
 
 # print(btn_click.text)
+driver.implicitly_wait(20)
+
+# WebElement 
+# y = driver.find_element(By.XPATH, "//a[contains(@id,'GridView3_lbView_0')]").click()
+
+# WebElement 
+# x = driver.find_element(By.XPATH, "//a[contains(@id,'GridView3_lbView_0')]").click()
+
+# print(x)
+# for x in driver.find_element(By.XPATH, "//a[contains(@id,'GridView3_lbView_0')]"):
+#     if x.text.isdigit():
+#         print(x.text)
 
 driver.switch_to.active_element
+driver.switch_to.window(driver.window_handles[0])
+# print(driver.page_source)
+time.sleep(0.4)
+# print(soup.select('span#lblNamaPendamping'))
+
+driver.implicitly_wait(20)
+result = driver.execute_script(
+"""
+  var data_pendamping = [];
+  for (var i of document.querySelectorAll('.modal#viewModalPPH')){
+     data_pendamping.push({
+        name:i.querySelector('span#lblNamaPendamping').textContent,
+        email:i.querySelector('span#lblEmailPendamping').textContent,
+        no_telp:i.querySelector('span#lblNoTelponPendamping').textContent,
+        pendamping_pelaku_usaha:i.querySelector('#gvData3').textContent,
+     });
+  }
+  return data_pendamping;
+"""
+)
+
+# r = pd.read_html(page.content)
+r = pd.read_html(driver.page_source)
+print(result)
 
 # Create var data_pendamping_pph as empty list
 # driver.switchTo().activeElement()
-parsed_code = BeautifulSoup(driver.page_source, 'html.parser').select('div#viewModalPPH')
+parsed_code = BeautifulSoup(driver.page_source, 'html.parser')
 # modal = driver.find_element(By.ID, "GridView3_lbView_0").click()
 
 # modal = driver.find_element(By.ID, 'viewModalPPH')
 # modal = parsed_code.select('div#viewModalPPH')
 
-print(parsed_code)
+# print(parsed_code)
 
 # modal_pendamping = modal.find('div', id='viewModalPPH')
 
